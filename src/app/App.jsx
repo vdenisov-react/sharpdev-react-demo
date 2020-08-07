14040c16f9cd58ce23079b83362eca07c94a5b50
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-07 08:40:28 +0300

    (routing) add routing for app and navigation for header

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { BrowserRouter as Router, NavLink, Route, Link, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+// layouts
 import { Header } from './layouts';
+
+// pages
+import { Auth, Home, Deals, Users } from './pages';
+
+const hashHistory = createBrowserHistory();
 
 function App() {
     return (
         <div className="app-root">
-            <Header />
+            <Router history={hashHistory}>
+                <Header />
+
+                <div className="content">
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/auth">
+                            <Auth />
+                        </Route>
+                        <Route path="/deals">
+                            <Deals />
+                        </Route>
+                        <Route path="/users">
+                            <Users />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
         </div>
     );
 }
--- src/app/layouts/header/header.controller.js
+++ src/app/layouts/header/header.controller.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import HeaderView from './header.view';
 
+const MENU = [
+    { key: 1, link: '/', label: 'Home' },
+    { key: 2, link: '/auth', label: 'Auth' },
+    { key: 3, link: '/deals', label: 'Deals' },
+    { key: 4, link: '/users', label: 'Users' },
+];
+
 export function Header() {
-    return <HeaderView />;
+    const title = 'PW React App';
+    const menu = MENU;
+
+    return <HeaderView ctrl={{ title, menu }} />;
 }
