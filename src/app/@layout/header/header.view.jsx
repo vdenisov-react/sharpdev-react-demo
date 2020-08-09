4a8d4a82b287e291e0c0f5f7cde29ed8a9b34210
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-09 22:00:00 +0300

    (navigation) restore toggle button func when small width

--- src/app/@layout/header/header.view.jsx
+++ src/app/@layout/header/header.view.jsx
@@ -10,7 +10,19 @@ export default ({ ctrl }) => (
                 {ctrl.appTitle}
             </span>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarColor01"
+                aria-controls="navbarColor01"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarColor01">
                 <ul className="navbar-nav mr-auto">
                     {ctrl.navMenu.map(item => (
                         <li key={item.key} className={'nav-item' + (item.link === ctrl.currentLink ? ' active' : '')}>
