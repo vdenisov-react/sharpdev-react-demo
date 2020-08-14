39191784acf2c6f4ac738974cdd4f48927e926b6
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-14 18:35:19 +0300

    (use-redux) enable extenstion "redux-devtools"

--- src/app/app.module.jsx
+++ src/app/app.module.jsx
@@ -11,7 +11,8 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { appReducer } from './@core/store/app.reducer';
 
-const appStore = createStore(appReducer);
+const extensionDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const appStore = createStore(appReducer, extensionDevTools);
 
 export default function AppModule() {
     return (
