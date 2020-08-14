9a4125a2caf61e9a1e317d0f765096bf20465b7d
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-14 17:42:25 +0300

    (use-redux) create store (+ actions, reducers, ...)

--- src/app/app.module.jsx
+++ src/app/app.module.jsx
@@ -6,6 +6,12 @@ import { Header } from './@layout';
 // routing
 import RouterOutlet from './app.routing';
 
+// store
+import { createStore } from 'redux';
+import { appReducer } from './@core/store/app.reducer';
+
+const store = createStore(appReducer);
+
 export default function AppModule() {
     return (
         <app-root>
