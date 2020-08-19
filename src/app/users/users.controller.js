a3dc85b2266bd29688b80419b9684cb7ac75f120
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-19 08:33:51 +0300

    [09-users-page] send request for getting users list when opening users page

--- src/app/users/users.controller.js
+++ src/app/users/users.controller.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { get } from 'lodash';
+
+// template
 import UsersView from './users.view';
+
+// services
+import { UsersService } from '../@core/api/services/users.service';
+
+// ...
+import { EMPTY_LINE_WITH_SINGLE_SPACE } from '../@shared/constants';
 
 export function Users() {
     const pageTitle = 'Users page';
+
+    useEffect(() => {
+        UsersService.getAll(EMPTY_LINE_WITH_SINGLE_SPACE)
+            .then(res => {
+                console.log('USERS =>', get(res, 'data'));
+            })
+            .catch(err => {
+                console.log('ERR =>', err);
+            });
+    }, []);
 
     return <UsersView ctrl={{ pageTitle }} />;
 }
