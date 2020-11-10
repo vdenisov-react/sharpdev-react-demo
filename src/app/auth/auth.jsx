0b02386e898912744d87cc8f93f268af6069f1cf
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-11-10 12:41:23 +0300

    [11-fix-issues] remove division into modules and routing for "app" and "auth"

--- src/app/auth/auth.jsx
+++ src/app/auth/auth.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
 
+// core
 import { history } from '../@core/navigation';
+
+// store
+import { thunkLogin, thunkRegister } from '../@core/store/auth';
 
 // pages
 import { Login } from './login/login.controller';
 import { Register } from './register/register.controller';
 
-export default ({
-    modulePath,
-    // ---
+function Auth({
     onLogin,
     errorLogin,
     // ---
     onRegister,
     errorRegister,
-}) => {
+}) {
+    const modulePath = '/auth';
+
     return (
         <div>
             <Router history={history}>
@@ -33,4 +38,22 @@ export default ({
             </Router>
         </div>
     );
-};
+}
+
+// ##################################################
+
+const mapStateToProps = ({ auth: authState }) => ({
+    errorLogin: authState.errorLogin,
+    errorRegister: authState.errorRegister,
+});
+
+const mapDispatchToProps = dispatch => ({
+    onLogin: (email, password) => {
+        return dispatch(thunkLogin(email, password));
+    },
+    onRegister: (email, username, password) => {
+        return dispatch(thunkRegister(email, username, password));
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
