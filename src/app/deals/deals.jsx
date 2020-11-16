340f39ed18aa8b14cd3f3476c6c96d368de5f45a
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-11-16 07:05:41 +0300

    [12-deals-page] move table of deals to separate component

--- src/app/deals/deals.jsx
+++ src/app/deals/deals.jsx
@@ -6,10 +6,10 @@ import { thunkAddNew, thunkGetList } from '../@core/store/deals';
 
 // styles
 import './deals.scss';
-import cn from 'classnames';
 
 // components
 import { AddingForm } from './adding-form/adding-form';
+import { DealsTable } from './deals-table/deals-table';
 
 function Deals({ dealsList, onAddNew, onGetList }) {
     useEffect(() => {
@@ -24,51 +24,7 @@ function Deals({ dealsList, onAddNew, onGetList }) {
         <app-deals>
             <AddingForm onCreateDeal={onCreateDeal} />
 
-            <div className="mt-3 deals-list">
-                {/* DEALS LIST */}
-                <table className="table">
-                    {/* table - header */}
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Username</th>
-                            <th scope="col">Amount</th>
-                            <th scope="col">Balance</th>
-                            <th scope="col">Date</th>
-                        </tr>
-                    </thead>
-
-                    {/* table - rows */}
-                    <tbody>
-                        {/* existing data */}
-                        {dealsList.length > 0 &&
-                            dealsList.map((deal, index) => (
-                                <tr
-                                    key={deal.id}
-                                    className={cn(
-                                        { 'table-success': deal.amount > 0 },
-                                        { 'table-danger': deal.amount < 0 },
-                                    )}
-                                >
-                                    <th scope="row">{index + 1}</th>
-                                    <td>{deal.username}</td>
-                                    <td>{deal.amount}</td>
-                                    <td>{deal.balance}</td>
-                                    <td>{deal.date}</td>
-                                </tr>
-                            ))}
-
-                        {/* no data */}
-                        {dealsList.length === 0 && (
-                            <tr>
-                                <td colSpan="100%">
-                                    <div className="no-data-msg">no data</div>
-                                </td>
-                            </tr>
-                        )}
-                    </tbody>
-                </table>
-            </div>
+            <DealsTable dealsList={dealsList} />
         </app-deals>
     );
 }
